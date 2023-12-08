2d32946ca6a0bcc2f807f8d017c50277712634ad
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2023-12-08 14:30:28 +0530

    Add files via upload

---
+++ Dev/src/test/java/Devstart/Dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package Devstart.Dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
